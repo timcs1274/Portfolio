0788446f93267faf63e4705a04e975ffd9b5809f
timcs <timcs1274@gmail.com>
2022-10-28 12:50:12 -0400

    create component boilerplates

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
