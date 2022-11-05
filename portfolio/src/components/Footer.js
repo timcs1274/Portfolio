97844488d2bcbbe07b87cedc298183fcf54ebe93
timcs <timcs1274@gmail.com>
2022-11-05 12:06:43 -0400

    import footer icons

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GithubIcon from '@material-ui/icons/Github';
+import styles from '../styles/Footer.css'
 
 function Footer() {
   return (
   <div className='footer'>
-    <div></div>
+    <div>
+        <LinkedInIcon />
+        <GithubIcon />
+    </div>
     <p> &copy; 2022 TimSasse </p>
   </div>
   );
