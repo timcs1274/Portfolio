733e732bc3c7589a1dd978ee8c86bc7f9b7afd03
timcs <timcs1274@gmail.com>
2022-11-05 13:04:52 -0400

    add external links to footer icons

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import '../styles/Footer.css'
@@ -7,8 +8,8 @@ function Footer() {
   return (
   <div className='footer'>
     <div className='footerIcons'>
-        <LinkedInIcon />
-        <GitHubIcon />
+        <Link to={{ pathname: "https://www.linkedin.com/in/timsasse/" }} target="_blank"><LinkedInIcon /></Link>
+        <Link to={{ pathname: "https://github.com/timcs1274" }} target="_blank"><GitHubIcon /></Link>
     </div>
     <p> &copy; 2022 TimSasse </p>
   </div>
