98b49e17c520f3d7a0f2adc0cbf4f5ef3396730f
timcs <timcs1274@gmail.com>
2022-11-07 09:45:39 -0500

    add basic home page classes

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='about'></div>
+        <h2>Tim</h2>
+        <div className='summary'>
+          <p>I like coding</p>
+        </div>
+    </div>
   )
 }
 
