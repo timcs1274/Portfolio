e587f43dda0ff844e617d35648a3a83eba965d9f
timcs <timcs1274@gmail.com>
2022-11-07 09:50:55 -0500

    add skills to home

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -3,11 +3,25 @@ import React from 'react'
 function Home() {
   return (
     <div className='home'>
-      <div className='about'></div>
+      <div className='about'>
         <h2>Tim</h2>
         <div className='summary'>
           <p>I like coding</p>
         </div>
+      </div>
+      <div className='skills'>
+        <h1>My Skills</h1>
+        <ol className='skillList'>
+          <li className='skillContent'>
+            <h2>Front-End</h2>
+            <p>React</p>
+          </li>
+          <li className='skillContent'>
+            <h2>Back-End</h2>
+            <p>Node.JS</p>
+          </li>
+        </ol>
+      </div>
     </div>
   )
 }
