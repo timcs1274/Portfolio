b9c833e47594a5ac510b1a5d5656b2c625d9f4e8
timcs <timcs1274@gmail.com>
2022-11-10 21:56:51 -0500

    add map function

--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import ProjectItem from '../components/ProjectItem';
+import { projectList } from '../helpers/ProjectList';
+import '../styles/Projects.css'
+
 
 function Projects() {
   return (
     <div className='projects'>
       <h1>My Projects</h1>
-      <div className='projectList'></div>
+      <div className='projectList'>
+        {projectList.map(() => {
+          return <ProjectItem name ={projectList.name} image={projectList.image}/>
+        })}
+      </div>
     </div>
   )
 }
