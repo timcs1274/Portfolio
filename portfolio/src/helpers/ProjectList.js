aaddf16d2003dbe6533b120460b551d9d3310860
timcs <timcs1274@gmail.com>
2022-11-17 12:55:41 -0500

    populate project list

--- portfolio/src/helpers/ProjectList.js
+++ portfolio/src/helpers/ProjectList.js
@@ -6,22 +6,22 @@ export const ProjectList = [
     {
         name: 'Seven At One Blow',
         image: project1,
-        skills: '',
-        summary: '',
-        link: ''
+        skills: 'MongoDB, Express, Apollo, Graphql, Bcrypt, React.js, terminal-in-react package',
+        summary: 'Seven At One Blow is a text adventure game which allows the user to indulge in an adaptation of the Brothers Grimm fairy tale.',
+        link: 'https://github.com/timcs1274/seven-at-one-blow'
     },
     {
         name: 'Date Night Bites',
         image: project2,
-        skills: '',
-        summary: '',
-        link: ''
+        skills: 'HTML, CSS, JavaScript, jQuery, Bulma, Edamam Recipe API, TheCocktailDB',
+        summary: 'Discover exciting recipes to cook with a loved one! Date Night Bites generates recipes based on user preference and a randomly paired cocktail to keep them on their toes.',
+        link: 'https://github.com/timcs1274/date-night-bites'
     },
     {
-        name: 'Note Taker',
+        name: 'Notes-Made-Easy',
         image: project3,
-        skills: '',
-        summary: '',
-        link: ''
+        skills: 'HTML, CSS, JavaScript, Express, Local Storage',
+        summary: 'Notes-Made-Easy is a simple note taker which locally stores the users notes',
+        link: 'https://github.com/timcs1274/Notes-Made-Easy'
     }
 ]
