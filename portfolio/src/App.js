11804ec19e6937ff32d23134a35defab98ee312d
timcs <timcs1274@gmail.com>
2022-11-17 12:17:44 -0500

    add project select component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,6 +5,7 @@ import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ProjectSelect from './pages/ProjectSelect';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/project/:id" element={<ProjectSelect />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
---
+++ portfolio/src/pages/ProjectSelect.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProjectSelect() {
+  return (
+    <div>ProjectSelect</div>
+  )
+}
+
+export default ProjectSelect
