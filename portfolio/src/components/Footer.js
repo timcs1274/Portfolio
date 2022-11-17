c50aab7407f24810fb3d87daa2f6f33719096664
timcs <timcs1274@gmail.com>
2022-11-17 13:12:27 -0500

    fix icon links

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import '../styles/Footer.css'
@@ -8,8 +7,8 @@ function Footer() {
   return (
   <div className='footer'>
     <div className='footerIcons'>
-        <Link to={{ pathname: "https://www.linkedin.com/in/timsasse/" }} target="_blank"><LinkedInIcon /></Link>
-        <Link to={{ pathname: "https://github.com/timcs1274" }} target="_blank"><GitHubIcon /></Link>
+        <a href='https://www.linkedin.com/in/timsasse/' target='_blank' style={{cursor: 'pointer'}} rel='noreferrer'><LinkedInIcon /></a>
+        <a href='https://github.com/timcs1274' target='_blank' style={{cursor: 'pointer'}} rel='noreferrer'><GitHubIcon /></a>
     </div>
     <p> &copy; 2022 TimSasse </p>
   </div>
