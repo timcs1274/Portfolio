1b093a54fb3e418f9e4f1e31dcf654ea3f394507
timcs <timcs1274@gmail.com>
2022-11-18 13:52:08 -0500

    add styling to project pages

--- portfolio/src/pages/ProjectSelect.js
+++ portfolio/src/pages/ProjectSelect.js
@@ -10,10 +10,8 @@ function ProjectSelect() {
     <div className='project'>
         <h1> {project.name}</h1>
         <img src={project.image} alt='project example'/>
-        <p>
-            {project.summary}
-            <b>Technologies:</b> {project.technologies}
-        </p>
+            <p className='summary'>{project.summary}</p>
+            <p className='tech'><b>Technologies:</b> {project.technologies}</p>
         <a href={project.link} target='_blank' style={{cursor: 'pointer'}} rel='noreferrer'>
             <button>Source Code</button>
         </a>
